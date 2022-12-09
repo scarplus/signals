6fcbb9f342f0f95fe981d2f3b64515d1350dff58
scarplus <112670649+scarplus@users.noreply.github.com>
2022-12-08 22:40:38 -0500

    Create sample_code_buy_trade.py

---
+++ tda/sample_code_buy_trade.py
@@ -0,0 +1,43 @@
+## dont forget to go and cancel the trade from your account after running this program.
+# this program will help place a buy order of 15 APPL stocks in your tda account
+
+
+
+import requests
+
+accountnumber='you tda actual account number'
+bearer_token='Your_bearer_token_key'
+
+# define the url
+url = f'https://api.tdameritrade.com/v1/accounts/{accountnumber}/orders'
+
+# define the payload
+payload = {
+  "orderType": "MARKET",
+  "session": "NORMAL",
+  "duration": "DAY",
+  "orderStrategyType": "SINGLE",
+  "orderLegCollection": [
+    {
+      "instruction": "Buy",
+      "quantity": 15,
+      "instrument": {
+        "symbol": "AAPL",
+        "assetType": "EQUITY"
+      }
+    }
+  ]
+}
+	
+Bearer_Token='Bearer '+Your_bearer_token_key
+# set the headers
+headers = {
+    'Authorization': Bearer_Token
+}
+
+# make the request
+r = requests.post(url=url, json=payload, headers=headers)
+
+print (r)
+
+# r value of 201 is success order placement
